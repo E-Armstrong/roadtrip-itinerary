d9e070ada1c75262a8dc50f591718e4f89e32e32
EricA <eggsaladsandwich@Es-Mac.local>
2017-11-05 22:59:02 -0700

    Can calculate total number of each cargo item on the server side and add up total cost and weights. Next step is to make the button responsive and then get the server to calculate the final results.

--- public/js/cargo-validator.js
+++ public/js/cargo-validator.js
@@ -1,36 +1,60 @@
 var mainVM = new Vue({
     el: "#app",
     data: {
-        totalWeight: 0,
-        totalCost: 0,
         totalBeer: 0,
         totalGatorade: 0,
         totalSoup: 0,
         totalTP: 0,
     },
+    //weights
+    // beer 18 lb $20
+    // soup 6 lb $15
+    // T.P. 14 lb $16
+    // Gatorade 35 lb $13
     methods: {
-        makeAMouse: function($event) {
-            //create marker at x and y
-            this.markers.push({
-                pageX: event.offsetX-10 + 'px',
-                pageY: event.offsetY-30 + 'px',
-            })
-            // return this.marker
-            // console.log($event)
-            // $event.pageX
-            // $event.page
-            // $event.bubbles: false
+        moreBeer: function($event) {
+            this.totalBeer++
         },
-        mouseTrap: function(mouse, index, event) {
-            event.stopPropagation()
-            this.markers.splice(index, 1)
-
-        }
+        lessBeer: function($event) {
+            this.totalBeer--
+            if (this.totalBeer < 0) {
+                this.totalBeer = 0
+            }
+        },
+        moreGatorade: function($event) {
+            this.totalGatorade++
+        },
+        lessGatorade: function($event) {
+            this.totalGatorade--
+            if (this.totalGatorade < 0) {
+                this.totalGatorade = 0
+            }
+        },
+        moreSoup: function($event) {
+            this.totalSoup++
+        },
+        lessSoup: function($event) {
+            this.totalSoup--
+            if (this.totalSoup < 0) {
+                this.totalSoup = 0
+            }
+        },
+        moreTP: function($event) {
+            this.totalTP++
+        },
+        lessTP: function($event) {
+            this.totalTP--
+            if (this.totalTP < 0) {
+                this.totalTP = 0
+            }
+        },
+    },
+    computed : {
+        totalWeight: function () {
+            return (this.totalBeer * 18) + (this.totalGatorade * 35) + (this.totalSoup * 6) + (this.totalTP * 14)
+        },
+        totalCost: function () {
+            return (this.totalBeer * 20) + (this.totalGatorade * 13) + (this.totalSoup * 15) + (this.totalTP * 16)
+        },
     }
 })
-
-//weights
-// beer 18 lb $20
-// soup 6 lb $15
-// T.P. 14 lb $16
-// Gatorade 35 lb $13
